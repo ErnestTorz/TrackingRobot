3f33f29e10b1a70d124e7b0b7c6d35cfb5e6d041
ErnestTorz <ernesttorz@gmail.com>
2022-04-21 14:26:08 +0200

    nowe

--- robot.py
+++ robot.py
@@ -1,116 +1,94 @@
-from time import sleep
-
 import RPi.GPIO as GPIO
-from numpy import int32
 
-in1 = 20 #in1
-in2 = 16 #in2
-ena = 21 #ena
+class Robot:
+    def __init__(self, ena, in1, in2, in3, in4, enb):
+        self.ena =ena
+        self.in1=in1
+        self.in2=in2
+        self.in3=in3
+        self.in4=in4
+        self.enb=enb
+        self.frequency=250
 
-in3 = 26 #in3
-in4 = 19 #in4
-enb = 13 #enb
-
-frequency=250
-
-temp1=1
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(ena,GPIO.OUT)
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-pwmA=GPIO.PWM(ena,frequency)
-
-pwmA.start(25)
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(in3,GPIO.OUT)
-GPIO.setup(in4,GPIO.OUT)
-GPIO.setup(enb,GPIO.OUT)
-GPIO.output(in3,GPIO.LOW)
-GPIO.output(in4,GPIO.LOW)
-pwmB=GPIO.PWM(enb,frequency)
-
-pwmB.start(25)
-print("\n")
-print("The default speed & direction of motor is LOW & Forward.....")
-print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-print("\n")    
-
-while(1):
-    
-    x=input()
-    
-    if x=='r':
-        print("run")
-        if(temp1==1):
-         GPIO.output(in2,GPIO.LOW)
-         GPIO.output(in3,GPIO.LOW)
-         GPIO.output(in1,GPIO.HIGH)
-         GPIO.output(in4,GPIO.HIGH)
-         print("forward")
-         x='z'
-        else:
-         GPIO.output(in1,GPIO.LOW)
-         GPIO.output(in4,GPIO.LOW)
-         GPIO.output(in2,GPIO.HIGH)
-         GPIO.output(in3,GPIO.HIGH)
-         print("backward")
-         x='z'
-
-
-    elif x=='s':
-        print("stop")
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(in1,GPIO.OUT)
+        GPIO.setup(in2,GPIO.OUT)
+        GPIO.setup(ena,GPIO.OUT)
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.LOW)
+        self.pwmA=GPIO.PWM(ena,self.frequency)
+        self.pwmA.start(0)
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(in3,GPIO.OUT)
+        GPIO.setup(in4,GPIO.OUT)
+        GPIO.setup(enb,GPIO.OUT)
         GPIO.output(in3,GPIO.LOW)
         GPIO.output(in4,GPIO.LOW)
-        x='z'
-
-    elif x=='f':
-        print("forward")
-        GPIO.output(in2,GPIO.LOW)
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in4,GPIO.HIGH)
-        temp1=1
-        x='z'
-
-    elif x=='b':
-        print("backward")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in4,GPIO.LOW)
-        GPIO.output(in2,GPIO.HIGH)
-        GPIO.output(in3,GPIO.HIGH)
-        temp1=0
-        x='z'
-
-    elif x=='l':
-        print("low")
-        pwmA.ChangeDutyCycle(25)
-        pwmB.ChangeDutyCycle(25)
-        x='z'
-
-    elif x=='m':
-        print("medium")
-        pwmA.ChangeDutyCycle(50)
-        pwmB.ChangeDutyCycle(50)
-        x='z'
-
-    elif x=='h':
-        print("high")
-        pwmA.ChangeDutyCycle(75)
-        pwmB.ChangeDutyCycle(75)
-        x='z'
-     
+        self.pwmB=GPIO.PWM(enb,self.frequency)
+        self.pwmB.start(0)
     
-    elif x=='e':
+    def __del__(self):
         GPIO.cleanup()
-        print("GPIO Clean up")
-        break
+
+    def stop (self):
+        GPIO.output(self.in1,GPIO.LOW)
+        GPIO.output(self.in2,GPIO.LOW)
+        GPIO.output(self.in3,GPIO.LOW)
+        GPIO.output(self.in4,GPIO.LOW)
+        self.pwmA.ChangeDutyCycle(0)
+        self.pwmB.ChangeDutyCycle(0)
+
+    def linear_drive(self, direction, speed):
+        if isinstance(speed,int) and (100 >= speed >= 0) :
+            if direction=="forward" or direction=="Forward" or direction=="f" or direction=="F" :
+                GPIO.output(self.in2,GPIO.LOW)
+                GPIO.output(self.in3,GPIO.LOW)
+                GPIO.output(self.in1,GPIO.HIGH)
+                GPIO.output(self.in4,GPIO.HIGH)
+                self.pwmA.ChangeDutyCycle(speed)
+                self.pwmB.ChangeDutyCycle(speed)
+            elif direction =="backward" or direction == "Backward" or direction=="b" or direction=="B" :
+                GPIO.output(self.in1,GPIO.LOW)
+                GPIO.output(self.in4,GPIO.LOW)
+                GPIO.output(self.in2,GPIO.HIGH)
+                GPIO.output(self.in3,GPIO.HIGH)
+                self.pwmA.ChangeDutyCycle(speed)
+                self.pwmB.ChangeDutyCycle(speed)
+
+    def rotation_in_place(self, direction, speed):
+        if isinstance(speed,int) and (100 >= speed >= 0) :
+            if direction=="left" or direction=="Left" or direction=="l" or direction=="L" :
+                GPIO.output(self.in2,GPIO.LOW)
+                GPIO.output(self.in4,GPIO.LOW)
+                GPIO.output(self.in1,GPIO.HIGH)
+                GPIO.output(self.in3,GPIO.HIGH)
+                self.pwmA.ChangeDutyCycle(speed)
+                self.pwmB.ChangeDutyCycle(speed)
+            elif direction =="right" or direction == "Right" or direction=="r" or direction=="R" :
+                GPIO.output(self.in1,GPIO.LOW)
+                GPIO.output(self.in3,GPIO.LOW)
+                GPIO.output(self.in2,GPIO.HIGH)
+                GPIO.output(self.in4,GPIO.HIGH)
+                self.pwmA.ChangeDutyCycle(speed)
+                self.pwmB.ChangeDutyCycle(speed)
+
+      ##Example use##
+import time
+robot=Robot(21,20,16,26,19,13)
+time.sleep(10)
+robot.linear_drive("f",30)
+time.sleep(4)
+robot.linear_drive("b",30)
+time.sleep(4)
+robot.rotation_in_place("l",40)
+time.sleep(5)
+robot.rotation_in_place("r",40)
+time.sleep(5)
+
     
-    else:
-        print("<<<  wrong data  >>>")
-        print("please enter the defined data to continue.....")
+
+        
+
+    
+
