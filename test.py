9e83d02c79d2877af747020121ab8dfa8160255d
ErnestTorz <ernesttorz@gmail.com>
2022-08-23 14:45:01 +0200

    nowe

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 #Import the necessary Packages for this software to run
+
 import mediapipe
 import cv2
 import hands_detector 
+from threading import Thread
+import copy
 
 hander= hands_detector.Hand_detector()
 
@@ -9,7 +12,8 @@ hander= hands_detector.Hand_detector()
 cap = cv2.VideoCapture(0)
 # fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 
-
+pom_thread = 0
+thread= None
 #Create an infinite loop which will produce the live feed to our desktop and that will search for hands
 while True:
            ret, frame = cap.read()
@@ -20,7 +24,28 @@ while True:
            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
            frame1 = cv2.resize(frame_rgb, (640, 480))
            
-           hander.detect(frame1)
+        #    wynik=hander.detect(frame1)
+           pom=-1
+           pom=hander.run(frame1)
+
+           if(pom==1):
+                    print("tak")
+           if(pom==-1):
+                    print("-1")
+         #   if(pom==0):
+         #             print("nie")
+        #    if (pom_thread == 0):   
+        #     thread = Thread(target=hander.detect, args=(copy.copy(frame1))).start()
+        #     pom_thread = 1
+
+        #    if pom_thread==1:
+        #     if ((Thread)(thread)).is_alive() == False:
+        #          if(hander.ret_val()==1):
+        #             print("tak")
+        #          else:
+        #              print("nie")
+        #          thread = Thread(target=hander.detect, args=(copy.copy(frame1))).start()
+           
            #Below shows the current frame to the desktop 
            cv2.imshow("Frame", frame);
            key = cv2.waitKey(1) & 0xFF
