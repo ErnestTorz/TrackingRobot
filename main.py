ae85173c3875ff26d85623ca2c67c0294138f45e
ErnestTorz <ernesttorz@gmail.com>
2022-08-31 17:28:16 +0200

    nowe

--- main.py
+++ main.py
@@ -179,74 +179,73 @@ while True:
     input_data = np.expand_dims(frame_resized, axis=0)
     
     if((time.time()-timer) < 5):
-    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-     if floating_model:
-        input_data = (np.float32(input_data) - input_mean) / input_std
+        # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+        if floating_model:
+            input_data = (np.float32(input_data) - input_mean) / input_std
 
-    # Perform the actual detection by running the model with the image as input
-     interpreter.set_tensor(input_details[0]['index'], input_data)
-     interpreter.invoke()
+        # Perform the actual detection by running the model with the image as input
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+        interpreter.invoke()
 
-    # Retrieve detection results
-     boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0]  # Bounding box coordinates of detected objects
-     classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]  # Class index of detected objects
-     scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0]  # Confidence of detected objects
+        # Retrieve detection results
+        boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0]  # Bounding box coordinates of detected objects
+        classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]  # Class index of detected objects
+        scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0]  # Confidence of detected objects
 
-    # Loop over all detections and draw detection box if confidence is above minimum threshold
-     for i in range(len(scores)):
-        if (scores[i] > min_conf_threshold) and (scores[i] <= 1.0) and (labels[int(classes[i])] =='person' or labels[int(classes[i])] == 'Person'):
+        # Loop over all detections and draw detection box if confidence is above minimum threshold
+        for i in range(len(scores)):
+            if (scores[i] > min_conf_threshold) and (scores[i] <= 1.0) and (labels[int(classes[i])] =='person' or labels[int(classes[i])] == 'Person'):
 
-            # Get bounding box coordinates and draw box
-            # Interpreter can return coordinates that are outside of image
-            # dimensions, need to force them to be within image using max() and min()
-            ymin = int(max(1, (boxes[i][0] * imH)))
-            xmin = int(max(1, (boxes[i][1] * imW)))
-            ymax = int(min(imH, (boxes[i][2] * imH)))
-            xmax = int(min(imW, (boxes[i][3] * imW)))
-            
-            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 255, 0), 2)
+                # Get bounding box coordinates and draw box
+                # Interpreter can return coordinates that are outside of image
+                # dimensions, need to force them to be within image using max() and min()
+                ymin = int(max(1, (boxes[i][0] * imH)))
+                xmin = int(max(1, (boxes[i][1] * imW)))
+                ymax = int(min(imH, (boxes[i][2] * imH)))
+                xmax = int(min(imW, (boxes[i][3] * imW)))
+                
+                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 255, 0), 2)
 
-            ## Draw label
-            object_name = labels[int(classes[i])]  # Look up object name from "labels" array using class index
-            label = '%s: %d%%' % (object_name, int(scores[i] * 100))  # Example: 'person: 72%'
-            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
-            label_ymin = max(ymin, labelSize[1] + 10)  # Make sure not to draw label too close to top of window
-            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1] - 10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)  # Draw white box to put label text in
-            cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)  # Draw label text
+                ## Draw label
+                object_name = labels[int(classes[i])]  # Look up object name from "labels" array using class index
+                label = '%s: %d%%' % (object_name, int(scores[i] * 100))  # Example: 'person: 72%'
+                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
+                label_ymin = max(ymin, labelSize[1] + 10)  # Make sure not to draw label too close to top of window
+                cv2.rectangle(frame, (xmin, label_ymin-labelSize[1] - 10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)  # Draw white box to put label text in
+                cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)  # Draw label text
 
-            # Draw circle in center of object
-            xcenter = xmin + (int(round((xmax - xmin) / 2)))
-            ycenter = ymin + (int(round((ymax - ymin) / 2)))
-            cv2.circle(frame, (xcenter, ycenter), 5, (255, 255, 0), thickness=-1)
-            detection_list.append(object(xcenter, ycenter, xmin, xmax, ymin, ymax))
-            
-     # Draw framerate in corner of frame
-     #  cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-    
-      
-     for i in detection_list:
-        if object_to_follow.distance(i) < min_distance and i.xcenter >= object_to_follow.xmin  and i.xcenter <= object_to_follow.xmax  and i.ycenter >= object_to_follow.ymin and i.ycenter <= object_to_follow.ymax:
-            next_object = i
-            min_distance = object_to_follow.distance(i)
-     detection_list.clear()
-    
-     if min_distance != math.inf:
-        timer=math.inf
-        find_flag=True
-        min_distance = math.inf
-        object_to_follow = next_object
-        cv2.circle(frame, (object_to_follow.xcenter, object_to_follow.ycenter), 5, (0, 0, 255), thickness=-1)
-        robot.run(imW, imH, object_to_follow.xcenter, object_to_follow.ycenter, object_to_follow.xmin, object_to_follow.xmax, object_to_follow.ymin, object_to_follow.ymax)
-     else:
-        if(find_flag==True):
-          timer=time.time()
-        find_flag=False
-        robot.stop()
+                # Draw circle in center of object
+                xcenter = xmin + (int(round((xmax - xmin) / 2)))
+                ycenter = ymin + (int(round((ymax - ymin) / 2)))
+                cv2.circle(frame, (xcenter, ycenter), 5, (255, 255, 0), thickness=-1)
+                detection_list.append(object(xcenter, ycenter, xmin, xmax, ymin, ymax))
+                
+        # Draw framerate in corner of frame
+        #  cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        
+        
+        for i in detection_list:
+            if object_to_follow.distance(i) < min_distance and i.xcenter >= object_to_follow.xmin  and i.xcenter <= object_to_follow.xmax  and i.ycenter >= object_to_follow.ymin and i.ycenter <= object_to_follow.ymax:
+                next_object = i
+                min_distance = object_to_follow.distance(i)
+        detection_list.clear()
+        
+        if min_distance != math.inf:
+            timer=math.inf
+            find_flag=True
+            min_distance = math.inf
+            object_to_follow = next_object
+            cv2.circle(frame, (object_to_follow.xcenter, object_to_follow.ycenter), 5, (0, 0, 255), thickness=-1)
+            robot.run(imW, imH, object_to_follow.xcenter, object_to_follow.ycenter, object_to_follow.xmin, object_to_follow.xmax, object_to_follow.ymin, object_to_follow.ymax)
+        else:
+            if(find_flag==True):
+              timer=time.time()
+            find_flag=False
+            robot.stop()
     else:
      object_to_follow=object(imH / 2, imW / 2, 0, imW, 0, imH)
-    #  croped=frame_rgb[0:imH,int(imW/5):int(imW*4/5)]
-     frame11 = cv2.resize(frame_rgb, (640, 480))
-     if((Gesture.detect(frame11))==1):
+     croped=frame_rgb[0:imH,int(imW/5):int(imW*4/5)]
+     if((Gesture.detect(croped))==1):
         timer=math.inf
         print("JEST!!!")
      else:
