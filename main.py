e3d28f625f0362d56ad4df1ae91663fc049f333d
Ernest Torz <ernesttorz@gmail.com>
2022-11-13 14:38:27 +0100

    new

--- main.py
+++ main.py
@@ -20,45 +20,43 @@ audio.mixer.init(26500)
 class VideoStream:
     """Camera object that controls video streaming from the Picamera"""
     def __init__(self, resolution=(640, 480), framerate=30):
-        # Initialize the PiCamera and the camera image stream
+        # Inicjalizacja streamu
         self.stream = cv2.VideoCapture(0)
         ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
         ret = self.stream.set(3, resolution[0])
         ret = self.stream.set(4, resolution[1])
             
-        ## Read first frame from the stream
+        # Odczytanie pierwszej klatki
         (self.grabbed, self.frame) = self.stream.read()
 
-        # Variable to control when the camera is stopped
+        # zmienna zapisujaca czy kamera jest zatrzymana
         self.stopped = False
 
     def start(self):
-        # Start the thread that reads frames from the video stream
+        # Wystrtowanie wątku, który odczytuje klatki
         Thread(target=self.update, args=()).start()
         return self
 
     def update(self):
-        # Keep looping indefinitely until the thread is stopped
-        while True:
-            # If the camera is stopped, stop the thread
+        while True:       
             if self.stopped:
-                # Close camera resources
+                # Zamkniecie kamery
                 self.stream.release()
                 return
 
-            # Otherwise, grab the next frame from the stream
+            # Odczytanie klatki
             (self.grabbed, self.frame) = self.stream.read()
 
     def read(self):
-        # Return the most recent frame
+        # odczytanie aktualnej klatki
         return self.frame
 
     def stop(self):
-        # Indicate that the camera and thread should be stopped
+        # Zatrzmanie wątku
         self.stopped = True
 
 
-# Define and parse input arguments
+# Argumenty
 parser = argparse.ArgumentParser()
 parser.add_argument('--modeldir', help='Folder the .tflite file is located in',
                     required=True)
@@ -68,7 +66,7 @@ parser.add_argument('--labels', help='Name of the labelmap file, if different th
                     default='labelmap.txt')
 parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
                     default=0.6)
-parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
+parser.add_argument('--resolution', help='Cammera resolution.',
                     default='360x300')  # 1280x720 tez dobra, 400x300
 parser.add_argument('--edgetpu', help='Use Coral Edge TPU Accelerator to speed up detection',
                     action='store_true')
@@ -83,9 +81,10 @@ resW, resH = args.resolution.split('x')
 imW, imH = int(resW), int(resH)
 use_TPU = args.edgetpu
 
-# Import TensorFlow libraries
-# If tflite_runtime is installed, import interpreter from tflite_runtime, else import from regular tensorflow
-# If using Coral Edge TPU, import the load_delegate library
+#Improtowanie bibliotek
+# Jesli tflite_runtime jest zainstalowany to import interpreter from tflite_runtime, 
+# inaczej import tensorflow
+# Jesli korzystamy z coral TPU to importujemy bibiloteke
 pkg = importlib.util.find_spec('tflite_runtime')
 if pkg:
     from tflite_runtime.interpreter import Interpreter
@@ -96,32 +95,30 @@ else:
     if use_TPU:
         from tensorflow.lite.python.interpreter import load_delegate
 
-# If using Edge TPU, assign filename for Edge TPU model
+
 if use_TPU:
-    # If user has specified the name of the .tflite file, use that name, otherwise use default 'edgetpu.tflite'
     if GRAPH_NAME == 'detect.tflite':
         GRAPH_NAME = 'edgetpu.tflite'       
 
-# Get path to current working directory
+# Pobranie scieżki do obecnego miejsca pracy
 CWD_PATH = os.getcwd()
 
-# Path to .tflite file, which contains the model that is used for object detection
+# Scieżka do modelu
 PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, GRAPH_NAME)
 
-# Path to label map file
+# Scieżka do pliku map
 PATH_TO_LABELS = os.path.join(CWD_PATH, MODEL_NAME, LABELMAP_NAME)
 
-# Load the label map
+# Załadownie label mapy
 with open(PATH_TO_LABELS, 'r') as f:
     labels = [line.strip() for line in f.readlines()]
 
-# Have to do a weird fix for label map if using the COCO "starter model" 
-# First label is '???', which has to be removed.
+#Fix
 if labels[0] == '???':
     del(labels[0])
 
-# Load the Tensorflow Lite model.
-# If using Edge TPU, use special load_delegate argument
+# Załadowanie modelu Tensorflow Lite.
+# Jesli korzystamy z  Edge TPU, używammy specialnego argumentu load_delegate
 if use_TPU:
     interpreter = Interpreter(model_path=PATH_TO_CKPT,
                               experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
@@ -131,7 +128,7 @@ else:
 
 interpreter.allocate_tensors()
 
-# Get model details
+# Infomracje o modelu
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 height = input_details[0]['shape'][1]
@@ -141,27 +138,25 @@ floating_model = (input_details[0]['dtype'] == np.float32)
 input_mean = 127.5
 input_std = 127.5
 
-# Check output layer name to determine if this model was created with TF2 or TF1,
-# because outputs are ordered differently for TF2 and TF1 models
+# Sprawsdzenie czy TF1 czy TF2
 outname = output_details[0]['name']
 
-if 'StatefulPartitionedCall' in outname:  # This is a TF2 model
+if 'StatefulPartitionedCall' in outname:  # TF2 model
     boxes_idx, classes_idx, scores_idx = 1, 3, 0
-else:  # This is a TF1 model
+else:  #TF1 model
     boxes_idx, classes_idx, scores_idx = 0, 1, 2
 
-# Initialize frame rate calculation
+# Inicjalizacja liczenia klatek
 frame_rate_calc = 1
 freq = cv2.getTickFrequency()
 
-# Initialize video stream
+# Inicjalizacja video streamu
 videostream = VideoStream(resolution=(imW, imH), framerate=30).start()
 time.sleep(1)
 
 cv2.namedWindow("Robot",cv2.WINDOW_NORMAL)
-# cv2.setWindowProperty("Robot",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
 
-# For frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+
 detection_list = []
 object_to_follow = object(imH / 2, imW / 2, 0, imW, 0, imH)
 next_object = object(0, 0, 0, 0, 0, 0)
@@ -175,42 +170,41 @@ gesture_flag=False
 Kp = 0.5
 
 while True:
-    # Start timer (for calculating frame rate)
+    # Start timeru do liczenia kaltek
     t1 = cv2.getTickCount()
    
-    # Grab frame from video stream
+    # Odczytanie klatki
     frame1 = videostream.read()
 
-    # Acquire frame and resize to expected shape [1xHxWx3]
+    # Skopiowanie obrazu, zmienienie rozmiarów, oraz zmiana na RGB wyjściowe rozmiary to [1xHxWx3]
     frame = frame1.copy()
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame_resized = cv2.resize(frame_rgb, (width, height))
     input_data = np.expand_dims(frame_resized, axis=0)
     
-    # Jesli miedzy wykrywaniem celu nie bylo przestoju wiekszego niz 5 sek (jesli prawda nie mozna uznac ze zgubiono cel) lub sledzona osoba wykonala gest w celu 
-    # przestania sledzenia
+   # Jeśli miedzy wykrywaniem celu nie było przestoju większego niż 5 sek 
+    #lub śledzona osoba wykonała gest w celu przestania śledzenia
     if((time.time()-timer) < 5):
-        # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+        
         if floating_model:
             input_data = (np.float32(input_data) - input_mean) / input_std
 
-        # Perform the actual detection by running the model with the image as input
+        # Wprowadzenie danych do interpretera 
         interpreter.set_tensor(input_details[0]['index'], input_data)
         interpreter.invoke()
 
-        # Retrieve detection results
-        boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0]  # Bounding box coordinates of detected objects
-        classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]  # Class index of detected objects
-        scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0]  # Confidence of detected objects
+         # Rezulataty detekcji
+        boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0] #Tablica ramek danego obiektu
+        classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]#Tablica klasy danego
+        scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0] #Tablica pewności co do 
+                                                                                #klasy danego obiektu
 
-        # Loop over all detections and draw detection box if confidence is above minimum threshold
+       # Przejście po wszystkich wynikach i wyrysowanie ich
         for i in range(len(scores)):
             if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0) 
                 and (labels[int(classes[i])] =='person' or labels[int(classes[i])] == 'Person')):
 
-                # Get bounding box coordinates and draw box
-                # Interpreter can return coordinates that are outside of image
-                # dimensions, need to force them to be within image using max() and min()
+                 # Odczytanie wspołrzędnych ramki oraz upewnienie się, że znajdują się wewnątrz obrazu
                 ymin = int(max(1, (boxes[i][0] * imH)))
                 xmin = int(max(1, (boxes[i][1] * imW)))
                 ymax = int(min(imH, (boxes[i][2] * imH)))
@@ -218,22 +212,20 @@ while True:
                 
                 cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 255, 0), 2)
 
-                ## Draw label
-                object_name = labels[int(classes[i])]  # Look up object name from "labels" array using class index
-                label = '%s: %d%%' % (object_name, int(scores[i] * 100))  # Example: 'person: 72%'
-                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
-                label_ymin = max(ymin, labelSize[1] + 10)  # Make sure not to draw label too close to top of window
+                 # Rysowanie nazwy klasy
+                object_name = labels[int(classes[i])]   # Znalezienie klasy obiektu
+                label = '%s: %d%%' % (object_name, int(scores[i] * 100))  
+                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) 
+                label_ymin = max(ymin, labelSize[1] + 10)  #Offset do napisu nad ramką
                 cv2.rectangle(frame, (xmin, label_ymin-labelSize[1] - 10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)  # Draw white box to put label text in
-                cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)  # Draw label text
-
-                # Draw circle in center of object
+                cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) 
+                 # Rysowanie okręgu na środku ramki
                 xcenter = xmin + (int(round((xmax - xmin) / 2)))
                 ycenter = ymin + (int(round((ymax - ymin) / 2)))
                 cv2.circle(frame, (xcenter, ycenter), 5, (255, 255, 0), thickness=-1)
                 detection_list.append(object(xcenter, ycenter, xmin, xmax, ymin, ymax))
                 
-        # Draw framerate in corner of frame
-        #  cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+       
         
         
         for i in detection_list:
